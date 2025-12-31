f60a6074d95750ae40571ab812dcf4d6fd63640e
huseynmohameda <huseynmohamedabdi@gmail.com>
2025-12-31 14:08:27 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,298 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama_1",
+      "name": "Panorama_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.36185254985703,
+          "pitch": 0.06399512419711861,
+          "rotation": 0,
+          "target": "1-panorama_2"
+        },
+        {
+          "yaw": 0.9416917442646202,
+          "pitch": 0.1039379350816585,
+          "rotation": 0,
+          "target": "4-panorama_5"
+        },
+        {
+          "yaw": -0.31937455087463107,
+          "pitch": -0.008576539657374482,
+          "rotation": 0,
+          "target": "3-panorama_4"
+        },
+        {
+          "yaw": -0.7215143991331683,
+          "pitch": 0.04295759545411926,
+          "rotation": 0,
+          "target": "5-panorama"
+        },
+        {
+          "yaw": 0.06632815614518783,
+          "pitch": 0.058735355916084586,
+          "rotation": 0,
+          "target": "2-panorama_3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-panorama_2",
+      "name": "Panorama_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2152101184271125,
+          "pitch": 0.0018416711836160005,
+          "rotation": 0,
+          "target": "0-panorama_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-panorama_3",
+      "name": "Panorama_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3899973565760888,
+          "pitch": -0.0016993082770664358,
+          "rotation": 0,
+          "target": "0-panorama_1"
+        },
+        {
+          "yaw": -1.0932672827683518,
+          "pitch": 0.07101895933099556,
+          "rotation": 0,
+          "target": "1-panorama_2"
+        },
+        {
+          "yaw": 1.2063116495063397,
+          "pitch": -0.020713419263854504,
+          "rotation": 0,
+          "target": "3-panorama_4"
+        },
+        {
+          "yaw": 0.41011348493316113,
+          "pitch": 0.07855368536958096,
+          "rotation": 0,
+          "target": "5-panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-panorama_4",
+      "name": "Panorama_4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22458868777947316,
+          "pitch": 0.07963984579611783,
+          "rotation": 0,
+          "target": "4-panorama_5"
+        },
+        {
+          "yaw": -0.8931467675911264,
+          "pitch": 0.1598513094233862,
+          "rotation": 0,
+          "target": "2-panorama_3"
+        },
+        {
+          "yaw": 0.14638686265595346,
+          "pitch": 0.03129700857901163,
+          "rotation": 0,
+          "target": "1-panorama_2"
+        },
+        {
+          "yaw": 0.5868809795585896,
+          "pitch": 0.06163196672092219,
+          "rotation": 0,
+          "target": "0-panorama_1"
+        },
+        {
+          "yaw": 1.1382345736057928,
+          "pitch": 0.11145423465730175,
+          "rotation": 0,
+          "target": "5-panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-panorama_5",
+      "name": "Panorama_5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0743236056930154,
+          "pitch": 0.016196562111016277,
+          "rotation": 0,
+          "target": "0-panorama_1"
+        },
+        {
+          "yaw": -1.9215905680533023,
+          "pitch": 0.090244930011675,
+          "rotation": 0,
+          "target": "1-panorama_2"
+        },
+        {
+          "yaw": 0.5439256076087275,
+          "pitch": 0.04763938475381835,
+          "rotation": 0,
+          "target": "2-panorama_3"
+        },
+        {
+          "yaw": 0.1836440227487266,
+          "pitch": -0.02955749472608815,
+          "rotation": 0,
+          "target": "3-panorama_4"
+        },
+        {
+          "yaw": -0.3058901063901178,
+          "pitch": 0.0455442487712201,
+          "rotation": 0,
+          "target": "4-panorama_5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-panorama",
+      "name": "Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
